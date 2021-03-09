78231172c3d2d2f65c79bfb3915268dfa54f1702
Gustavo Matias dos Santos <gustavo@matias.love>
2021-03-09 01:29:47 +0000

    Create Home, About and Navigator components with route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,154 +1,69 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './fontawesome';
-//import { library  } from '@fortawesome/fontawesome-svg-core';
 import './clean-blog.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Navigator from './Navigator';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container">
-          <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="post.html">Sample Post</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <Router>
+      <div className="App">
+        <Navigator/>
 
-      <header class="masthead" style={{ backgroundImage: "url('img/home-bg.jpg')" }}>
-        <div class="overlay"></div>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <div class="site-heading">
-                <h1>Clean Blog</h1>
-                <span class="subheading">A Blog Theme by Start Bootstrap</span>
+        <Switch>
+          <Route path="/home"><Home/></Route>
+          <Route path="/about"><About/></Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/post">Post</Route>
+          <Route path="/"><Home/></Route>
+        </Switch>
+
+        <hr/>
+
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 col-md-10 mx-auto">
+                <ul class="list-inline text-center">
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+                <p class="copyright text-muted">Copyright &copy; Your Website 2020</p>
               </div>
             </div>
           </div>
-        </div>
-      </header>
-
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 class="post-subtitle">
-                  Problems look mighty small from 150 miles up
-                </h3>
-              </a>
-              <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 24, 2019</p>
-            </div>
-            <hr/>
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                </h2>
-              </a>
-              <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 18, 2019</p>
-            </div>
-            <hr/>
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  Science has not yet mastered prophecy
-                </h2>
-                <h3 class="post-subtitle">
-                  We predict too much for the next year and yet far too little for the next ten.
-                </h3>
-              </a>
-              <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on August 24, 2019</p>
-            </div>
-            <hr/>
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  Failure is not an option
-                </h2>
-                <h3 class="post-subtitle">
-                  Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                </h3>
-              </a>
-              <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on July 8, 2019</p>
-            </div>
-            <hr/>
-            <div class="clearfix">
-              <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-            </div>
-          </div>
-        </div>
+        </footer>
       </div>
-
-      <hr/>
-
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <ul class="list-inline text-center">
-                <li class="list-inline-item">
-                  <a href="#">
-                    <span class="fa-stack fa-lg">
-                      <i class="fas fa-circle fa-stack-2x"></i>
-                      <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <span class="fa-stack fa-lg">
-                      <i class="fas fa-circle fa-stack-2x"></i>
-                      <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <span class="fa-stack fa-lg">
-                      <i class="fas fa-circle fa-stack-2x"></i>
-                      <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-              <p class="copyright text-muted">Copyright &copy; Your Website 2020</p>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/Navigator.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Header () {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <Link class="nav-link" to="/home">Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/about">About</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/post">Sample Post</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
