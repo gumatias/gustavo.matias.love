e2b1788b83095e4182a573444d4719b1547d7887
Gustavo Matias dos Santos <gustavo@matias.love>
2021-04-12 23:58:12 +0000

    Return gumatias88 HN data in backend

--- app/controllers/api/v1/hacker_news/user_comments_controller.rb
+++ app/controllers/api/v1/hacker_news/user_comments_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::HackerNews::UserCommentsController < ApplicationController
   def index
-    render json: { test: "funciona!" }
+    raw_response = Faraday.get("https://hacker-news.firebaseio.com/v0/user/gumatias88.json")
+    user_comments = JSON.parse(raw_response.body)
+
+    render json: user_comments
   end
 end
