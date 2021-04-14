d366bd5e41c2d815bcb2b45d01c415c0be03d294
Gustavo Matias dos Santos <gustavo@matias.love>
2021-04-14 03:32:17 +0000

    Respond with gumatias88 HackerNew user comments

--- app/controllers/api/v1/hacker_news/user_comments_controller.rb
+++ app/controllers/api/v1/hacker_news/user_comments_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::HackerNews::UserCommentsController < ApplicationController
   def index
     raw_response = Faraday.get("https://hacker-news.firebaseio.com/v0/user/gumatias88.json")
-    user_comments = JSON.parse(raw_response.body)
+    user_profile = JSON.parse(raw_response.body)
 
-    render json: user_comments
+    comments = user_profile["submitted"].map do |submission_id|
+      raw_response = Faraday.get("https://hacker-news.firebaseio.com/v0/item/#{submission_id}.json")
+      JSON.parse(raw_response.body)
+    end
+
+    render json: comments
   end
 end
